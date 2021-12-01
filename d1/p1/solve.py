31a171de909e0b02c2b9468afb269fecbff6dd22
h0useofdupree <joel.riekemann@gmail.com>
2021-12-01 14:56:04 +0100

    FC

---
+++ d1/p1/solve.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Advent of Code 2021, day 1, part 1
+# https://adventofcode.com/2021/day/1
+# Author: Joel Riekemann
+# Date: 2021-12-01
+
+# Read input into a list of integers
+with open("input.txt") as f:
+    numbers = [int(line.strip()) for line in f]
+
+
+# Output lines
+#print(f"Here are the lines: {numbers}")
+
+index = 0
+count = 0
+
+# Count every number but the first if it is bigger than the preceding one
+for line in numbers:
+    if index > 0:
+        if line > numbers[index - 1]:
+            count += 1
+    index += 1
+print(count)
---
+++ d1/p2/solve.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Advent of Code 2021, day 1, part 2
+# https://adventofcode.com/2021/day/1#part2
+# Author: Joel Riekemann
+# Date: 2021-12-01
+
+# Read input
+with open("input.txt") as f:
+    lines = [int(line.strip()) for line in f]
+
+index = 0
+count = 0
+clusters = []
+
+for l in lines:
+    clusters.append(sum(lines[index:(index+3)]))
+    index += 1
+index = 0
+
+for c in clusters:
+    if index > 0:
+        if clusters[index] > clusters[index - 1]:
+            count += 1
+    index += 1
+
+print(count)
